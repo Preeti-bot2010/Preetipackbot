b3402ae4fd231f3bdfdf0e8f77dc92eabfc1647f
Preeti-bot2010 <hsakriya@gmail.com>
2025-06-15 12:47:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,10 +4,8 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from 'dist' folder
 app.use(express.static(path.join(__dirname, 'dist')));
 
-// Fallback route for single-page apps
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
