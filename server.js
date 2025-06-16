c25ed9a46a124529d3e1b5ff038f91a7bf20beb7
Preeti-bot2010 <hsakriya@gmail.com>
2025-06-16 13:41:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.post("/webhook", express.json(), (req, res) => {
 });
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname,'index.html'));
+  res.sendFile(path.join(__dirname,"dist",'index.html'));
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
